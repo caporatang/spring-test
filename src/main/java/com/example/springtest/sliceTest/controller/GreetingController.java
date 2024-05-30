ecbd70cd480b13051df10486460c378aa10560c4
caporatang <caporatang@naver.com>
2024-05-31 00:23:12 +0900

    Feat : sliceTest helper 클래스 작성
    R2DBC 의존성 추가
    GreetingController, GreetingService, UserEntity, UserR2dbcRespository 작성

---
+++ src/main/java/com/example/springtest/sliceTest/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.springtest.sliceTest.controller;
+
+import com.example.springtest.sliceTest.service.GreetingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    private final GreetingService greetingService;
+
+    public Mono<String> greetinmg(
+            @RequestParam String who
+    ) {
+        return greetingService.greetingMono(who);
+    }
+}
