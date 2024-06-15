9259d707c2b9b9e5e6b3ef8dbef57cd8630111e1
caporatang <caporatang@naver.com>
2024-06-16 00:08:07 +0900

    Chore : 전체 파일 디렉토리 이동

--- src/main/java/com/example/springtest/sliceTest/domain/UserR2dbcRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springtest.sliceTest.domain;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import reactor.core.publisher.Mono;
-
-public interface UserR2dbcRepository extends R2dbcRepository<UserEntity, Long> {
-    Mono<UserEntity> findByName(String name);
-}
